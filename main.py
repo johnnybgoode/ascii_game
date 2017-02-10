dea61c3c6775cb4fe79747f800c5f4139e13c774
John Entwistle <john@thirdandgrove.com>
2017-02-09 23:55:06 -0500

    initial working threading

--- main.py
+++ main.py
@@ -33,89 +33,66 @@ def main(win):
   def control(commands, game):
     while not game.over:
       command = game.win.getch()
-      if (command < 0):
+      if command < 0:
         continue
 
       command = chr(command)
       commands.put(command)
 
+      game.player.move(command)
+      #game.score += 1
+
       if command == 'q':
         game.over = True
         break
 
-  def ai(commands, game):
+  def enemies(game):
     while not game.over:
       for enemy in game.enemies:
         enemy.move(game.player.get_pos())
 
-      time.sleep(2)
+      time.sleep(0.5)
 
   def display(commands, game):
     while not game.over:
+      game.win.clear()
+
       try:
         command = commands.get(False)
       except Queue.Empty, e:
         command = ""
 
-      game.win.addstr(game.board.size_y +4, 0, command)
 
       if command == 'q':
         game.over = True
         break
 
-      game.player.move(command)
+      for enemy in game.enemies:
+        if (game.player.get_pos() == enemy.get_pos()):
+          game.board.game_over()
+          game.over = True
+          break
 
-      #game.score += 1
-
-      #for enemy in game.enemies:
-      #  if (game.player.get_pos() == enemy.get_pos()):
-      #    game.board.game_over()
-      #    game.over = True
-      #    break
-
-      game.win.clear()
       game.board.draw()
       win.addstr(game.board.size_y + 2, 0, 'Score: ' + str(game.score))
 
-      time.sleep(1)
+      time.sleep(0.3)
 
-  #while key != ord('q') and not game_over:
-  #  key = win.getch()
-  #  if (key < 0):
-  #    key = 0
-
-
-  #  #time.sleep(2)
-
-  #  for enemy in enemies:
-  #    enemy.move(player.get_pos())
-
-  #  win.clear()
-  #  board.draw()
-  #  win.addstr(board.size_y + 2, 0, 'Score: ' + str(score))
-
-  #  for enemy in enemies:
-  #    if (player.get_pos() == enemy.get_pos()):
-  #      board.game_over()
-  #      game_over = True
-  #      break
-
-  #  score += 1
-
-  #pdb.set_trace()
-  # then start the two threads
-  displayer = threading.Thread(None, # always to None since the ThreadGroup class is not implemented yet
-                              display, # the function the thread will run
-                              None, # doo, don't remember and too lazy to look in the doc
-                              (commands, game), # *args to pass to the function
-                               {}) # **kwargs to pass to the function
+  displayer = threading.Thread(None, display, None, (commands, game), {})
 
   controler = threading.Thread(None, control, None, (commands, game), {})
 
-  aier = threading.Thread(None, ai, None, (game,), {})
+  ai = threading.Thread(None, enemies, None, (game,), {})
 
   displayer.start()
   controler.start()
-  aier.start()
+  ai.start()
+
+  if (game.over):
+    win.nodelay(False)
+
+    key = 0
+    while chr(key) != 'q':
+      key = win.getch()
 
 curses.wrapper(main)
