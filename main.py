0cc27452b3e92c45838e2caec6892959544fbf3f
John Entwistle <john@thirdandgrove.com>
2017-03-10 12:55:08 -0500

    Move global game vars and main loop to Game object

--- main.py
+++ main.py
@@ -1,54 +1,11 @@
 from __future__ import division
-import curses, math, time
-from board import Board
-from player import Player
-from enemy import Enemy
+import curses
+from game import Game
 
 def main(win):
-  win.nodelay(True)
-
-  board = Board(win, 'board.txt')
-
-  frame = 1/60
-  game_over = False
-  score = 0
-
-  player = Player(board)
-  enemies = [
-    Enemy(board, 60),
-    Enemy(board, 65),
-  ]
-
-  win.clear()
-  board.draw()
-  win.addstr(board.size_y + 2, 0, 'Score: ' + str(score))
-
-  key = 0
-
-  while key != ord('q') and not game_over:
-    key = win.getch()
-
-    if (key > 0):
-      player.move(chr(key))
-
-    for enemy in enemies:
-      enemy.move(player.get_pos())
-
-    win.erase()
-    board.draw()
-    win.addstr(board.size_y + 2, 0, 'Score: ' + str(int(math.floor(score))))
-    win.refresh()
-
-    for enemy in enemies:
-      if (player.get_pos() == enemy.get_pos()):
-        board.game_over()
-        game_over = True
-        break
-
-    score += frame
-    time.sleep(frame)
-
-  while key != ord('q'):
-    key = win.getch()
+  game = Game(win)
+  game.spawn(5)
+  game.play()
+  #win.addstr(board.size_y + 2, 0, 'Score: ' + str(score))
 
 curses.wrapper(main)
