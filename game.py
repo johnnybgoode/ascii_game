a7d1ded612b306188ffddbf7384d199809135dca
John Entwistle <john@thirdandgrove.com>
2017-03-11 12:25:43 -0500

    Move render logic from Board to Game

--- game.py
+++ game.py
@@ -1,14 +1,13 @@
-from __future__ import division
-import time
-import pdb
+import random, math, time
+
 from board import Board
 from player import Player
 from enemy import Enemy
 
 class Game:
   def __init__(self, win):
-    self.board = Board(win, "board.txt")
     self.framerate = 1/60
+    self.board = Board("board.txt")
     self.over = False
     self.score = 0
     self.win = win;
@@ -19,14 +18,12 @@ class Game:
     self.enemies = []
 
   def play(self):
-    win = self.win
-
-    self.board.draw()
+    self.draw()
 
     key = 0
 
     while key != ord('q') and not self.over:
-      key = win.getch()
+      key = self.win.getch()
 
       if key > 0:
         self.player.move(chr(key))
@@ -34,19 +31,21 @@ class Game:
       for enemy in self.enemies:
         enemy.move(self.player.get_pos())
 
-      win.refresh()
+      self.draw()
 
       for enemy in self.enemies:
         if (self.player.get_pos() == enemy.get_pos()):
           self.over = True
+          self.show_message('Game Over', (0,2))
           break
 
       self.score += self.framerate
+      self.show_message('Score: ' + str(self.seconds))
 
       time.sleep(self.framerate)
 
     while key != ord('q'):
-      key = win.getch();
+      key = self.win.getch();
 
   def spawn(self, n):
     i = 0;
@@ -58,3 +57,18 @@ class Game:
       self.enemies.append(enemy)
       i += 1
 
+  def draw(self):
+    board = self.board.get_board()
+    for i in range(self.board.size_y):
+      for j in range(self.board.size_x):
+        self.win.addstr(i, j, str(board[i][j]))
+    self.win.refresh()
+
+  def show_message(self, message, offset = (0,0)):
+    self.win.move(self.board.size_y + 1, 0)
+    self.win.clrtoeol()
+
+    x = math.floor((self.board.size_x / 2) - (len(message) / 2))
+    x += offset[0]
+    y = self.board.size_y + offset[1]
+    self.win.addstr(self.board.size_y, int(x), str(message))
